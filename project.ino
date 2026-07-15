b1cb8160970c5eeab7922777e9447dcc23131118
FNAF0316-afk <FNAF0316@icloud.com>
2026-07-15 15:41:08 +0800

    Update project.ino

--- project.ino
+++ project.ino
@@ -1,19 +1,15 @@
-const int trigPin = 6;
-const int echoPin = 7;
+const int trigPin = 5;
+const int echoPin = 18;
 float duration, distance;
 void setup() {
   pinMode(23, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(17,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(23, HIGH);
-  delay(1000);
-  digitalWrite(23, LOW);
-  delay(1000);
-
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -21,8 +17,23 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration=pulseIn(echoPin, HIGH);
   distance=duration*0.034/2;
-  Serial.print("destance");
+  Serial.print("distance");
   Serial.print(distance);
   Serial.println("cm");
   delay(500);
+  if(distance<=20){
+    digitalWrite(23, HIGH);
+    delay(1000);
+    digitalWrite(23, LOW);
+    delay(3000);
+  }
+    if(digitalRead(17)==LOW){
+      digitalWrite(23, HIGH);
+      delay(1000);
+    }
+    else{
+      digitalWrite(23, LOW);
+    }
+  }
+
 }
